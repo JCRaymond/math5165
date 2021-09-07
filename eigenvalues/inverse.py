2fe94bd767491208ac2974526c979710149d425e
Jacob Raymond <raymond.jacob17@gmail.com>
2021-09-07 18:56:58 -0400

    Implement inverse iteration, and make rayleigh file

--- eigenvalues/inverse.py
+++ eigenvalues/inverse.py
@@ -20,21 +20,34 @@ def power_eigenvec(A, epsilon=1e-6):
       if dist < epsilon:
          return x
 
+def inverse_eigenvec(A, epsilon=1e-6):
+   if len(A.shape) != 2 or A.shape[0] != A.shape[1] or np.linalg.det(A) == 0:
+      return None
+
+   A_inv = np.linalg.inv(A)
+   return power_eigenvec(A_inv, epsilon)
+
 def power_e_val_vec(A, epsilon=1e-6):
    e_vec = power_eigenvec(A)
    val_vec = (A*e_vec)/e_vec
    e_val = max(val_vec.max(), val_vec.min(), key=abs)
    return e_val, e_vec
 
+def inverse_e_val_vec(A, epsilon=1e-6):
+   if len(A.shape) != 2 or A.shape[0] != A.shape[1] or np.linalg.det(A) == 0:
+      return None
+
+   A_inv = np.linalg.inv(A)
+   val, vec = power_e_val_vec(A_inv)
+   return 1/val, vec
 
 if __name__ == "__main__":
-   A = np.matrix([
-      [4, 0, 0],
-      [0, 1, 0],
-      [0, 0, -5]
-   ])
 
-   val, vec = power_e_val_vec(A)
+   A = np.matrix([
+      [1, 2, 3],
+      [4, 5, 4],
+      [3, 2, 1]
+   ])
+   val, vec = inverse_e_val_vec(A)
 
    print(val, vec)
-
---
+++ eigenvalues/rayleigh_inverse.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3.9
+
+import numpy as np
+
+def normalize(vec):
+   return vec / np.linalg.norm(vec)
+
+def power_eigenvec(A, epsilon=1e-6):
+   if len(A.shape) != 2 or A.shape[0] != A.shape[1]:
+      return None
+
+   n = A.shape[0]
+   x = normalize([np.ones(n)]).T
+   
+   while True:
+      x_old = x
+      x = normalize(A * (A * x))
+
+      dist = np.linalg.norm(x - x_old, ord=float('inf'))
+      if dist < epsilon:
+         return x
+
+def inverse_eigenvec(A, epsilon=1e-6):
+   if len(A.shape) != 2 or A.shape[0] != A.shape[1] or np.linalg.det(A) == 0:
+      return None
+
+   A_inv = np.linalg.inv(A)
+   return power_eigenvec(A_inv, epsilon)
+
+def rayleigh_eigenvec(A, epsilon=1e-6):
+   x = normalize([np.ones(n)]).T
+   rayleigh_quot = (x.T * (A * x))/(x.T * x)
+   
+   shifted_A = A - rayleigh_quot*np.eye(like = A)
+
+   e_vec = inverse_eigenvec(
+   
+
+def power_e_val_vec(A, epsilon=1e-6):
+   e_vec = power_eigenvec(A)
+   val_vec = (A*e_vec)/e_vec
+   e_val = max(val_vec.max(), val_vec.min(), key=abs)
+   return e_val, e_vec
+
+def inverse_e_val_vec(A, epsilon=1e-6):
+   if len(A.shape) != 2 or A.shape[0] != A.shape[1] or np.linalg.det(A) == 0:
+      return None
+
+   A_inv = np.linalg.inv(A)
+   val, vec = power_e_val_vec(A_inv)
+   return 1/val, vec
+
+if __name__ == "__main__":
+
+   A = np.matrix([
+      [1, 2, 3],
+      [4, 5, 4],
+      [3, 2, 1]
+   ])
+   val, vec = inverse_e_val_vec(A)
+
+   print(val, vec)
