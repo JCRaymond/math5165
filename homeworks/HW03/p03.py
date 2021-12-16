b51a838034110e5bccb5d79d50886bc76fb4d0b1
Jacob Raymond <raymond.jacob17@gmail.com>
2021-12-16 16:55:41 -0500

    Implement stochastic GD. Get data for SGD

--- homeworks/HW03/p03.py
+++ homeworks/HW03/p03.py
@@ -21,7 +21,7 @@ class LogisticRegressionCost:
         self.X = X
         self.y = y
         self.d = X.shape[0]
-        sefl.n = X.shape[1]
+        self.n = X.shape[1]
 
     def grad_i(self, i, args):
         alpha = args[0]
@@ -39,14 +39,26 @@ class LogisticRegressionCost:
         grad = self.grad_i(0, args)
         for i in range(1, self.n):
             grad += self.grad_i(i, args)
+        grad /= self.n
         return grad
+
+
+class LogisticPredictor:
+    def __init__(self, args):
+        self.alpha = args[0]
+        self.beta = args[1:]
+
+    def __call__(self, x):
+        z = -self.alpha - np.dot(self.beta, x)
+        p = 1 / (1 + np.exp(z))
+        return 1 if p >= 0.5 else 0
 
 
 def stoch_grad_desc(cost,
                     x_0,
                     *,
                     train_perc=0.01,
-                    alpha=0.001,
+                    alpha=0.005,
                     epsilon=1e-7,
                     maxiters=1000000):
     idxs = np.arange(cost.n)
@@ -62,6 +74,51 @@ def stoch_grad_desc(cost,
     grad_x = cost.grad_i(step_idxs[0], x)
     for i in range(1, step_samples):
         grad_x += cost.grad_i(step_idxs[i], x)
+    grad_x /= step_samples
+    for i in range(maxiters):
+        x = x - alpha * grad_x
+        step_idxs = rng.choice(idxs,
+                               step_samples,
+                               replace=False,
+                               shuffle=False)
+        grad_x = cost.grad_i(step_idxs[0], x)
+        for j in range(1, step_samples):
+            grad_x += cost.grad_i(step_idxs[j], x)
+        grad_x /= step_samples
+        if la.norm(grad_x) < epsilon:
+            if stable_iters >= needed_stable_iters:
+                return x, i + 1
+            stable_iters += 1
+        else:
+            stable_iters = 0
+        if i % 100 == 0:
+            print(i, grad_x, stable_iters)
+    return x, maxiters
+
+
+def stoch_grad_desc_ridge(cost,
+                          x_0,
+                          *,
+                          mu=1,
+                          train_perc=0.01,
+                          alpha=0.001,
+                          epsilon=1e-7,
+                          maxiters=1000000):
+    idxs = np.arange(cost.n)
+    step_samples = int(train_perc * cost.n)
+    if step_samples < 1:
+        step_samples = 1
+
+    needed_stable_iters = int(np.sqrt(1 / train_perc)) + 1
+    stable_iters = 0
+
+    x = x_0
+    step_idxs = rng.choice(idxs, step_samples, replace=False, shuffle=False)
+    grad_x = cost.grad_i(step_idxs[0], x)
+    for i in range(1, step_samples):
+        grad_x += cost.grad_i(step_idxs[i], x)
+    grad_x /= step_samples
+    grad_x -= mu * x  # Ridge term
     for i in range(maxiters):
         x = x - alpha * grad_x
         step_idxs = rng.choice(idxs,
@@ -71,6 +128,8 @@ def stoch_grad_desc(cost,
         grad_x = cost.grad_i(step_idxs[0], x)
         for i in range(1, step_samples):
             grad_x += cost.grad_i(step_idxs[i], x)
+        grad_x /= step_samples
+        grad_x -= mu * x  # Ridge term
         if la.norm(grad_x) < epsilon:
             if stable_iters >= needed_stable_iters:
                 return x, i + 1
@@ -80,56 +139,51 @@ def stoch_grad_desc(cost,
     return x, maxiters
 
 
+def get_data(fname):
+    X = []
+    y = []
+    with open(fname, 'r') as f:
+        for line in f:
+            age, _, fnlwgt, _, education_num, _, _, _, _, sex, capital_gain, capital_loss, hours_per_week, _, salary = map(
+                str.strip, line.split(','))
+            age = int(age)
+            fnlwgt = int(fnlwgt)
+            education_num = int(education_num)
+            sex = 0 if sex == 'Male' else 1
+            capital_gain = int(capital_gain)
+            capital_loss = int(capital_loss)
+            hours_per_week = int(hours_per_week)
+            salary = 0 if salary == '<=50K' else 1
+            X.append(
+                np.array((age, fnlwgt, education_num, sex, capital_gain,
+                          capital_loss, hours_per_week)))
+            y.append(salary)
+
+    X = np.array(X, dtype=np.float64)
+    X = (X - X.min(axis=0))
+    X /= X.max(axis=0)
+    y = np.array(y)
+    return X.T, y
+
+
+def test():
+    X = np.array(((0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2))).T
+    y = np.array((0, 1, 1, 0, 0, 1))
+    cost = LogisticRegressionCost(X, y)
+    initial_weights = np.zeros(cost.d + 1)
+    optimal_weights, iters = stoch_grad_desc(cost, initial_weights)
+    pred = LogisticPredictor(optimal_weights)
+    for i in range(X.shape[1]):
+        print(pred(X[:, i]), y[i])
+
+
 def solve():
-    x_0 = np.zeros(2)
-    a = 2
-    epsilon = 1e-7
-    for b in (0.1, 1, 10, 25, 50, 100):
-        f = Rosenbrock(a, b)
-
-        t = timer()
-        x1, i1 = grad_desc(f.grad, x_0, alpha=0.001, epsilon=epsilon)
-        t1 = t.get_time()
-
-        t = timer()
-        x2, i2 = grad_desc_opt(f, x_0, epsilon=epsilon)
-        t2 = t.get_time()
-        print(f'=== Rosenbrock({a},{b}) ===')
-        print(f' - Basic -')
-        print(f'   time: {t1:.4e}')
-        print(f'  iters: {i1}')
-        print(f'   f(x): {f(x1):.4e}')
-        print(f'')
-        print(f' - Optimal -')
-        print(f'   time: {t2:.4e}')
-        print(f'  iters: {i2}')
-        print(f'   f(x): {f(x2):.4e}')
-        print(f'')
-
-    f = Himmelblau()
-
-    t = timer()
-    x1, i1 = grad_desc(f.grad, x_0, alpha=0.001, epsilon=epsilon)
-    t1 = t.get_time()
-
-    t = timer()
-    x2, i2 = grad_desc_opt(f, x_0, epsilon=epsilon)
-    t2 = t.get_time()
-
-    print(f'=== Himmelblau ===')
-    print(f' - Basic -')
-    print(f'   time: {t1:.4e}')
-    print(f'  iters: {i1}')
-    print(f'      x: {x1}')
-    print(f'   f(x): {f(x1):.4e}')
-    print(f'')
-    print(f' - Optimal -')
-    print(f'   time: {t2:.4e}')
-    print(f'  iters: {i2}')
-    print(f'      x: {x2}')
-    print(f'   f(x): {f(x2):.4e}')
-    print(f'')
+    X, y = get_data('adult_train.csv')
+    cost = LogisticRegressionCost(X, y)
+    initial_weights = np.ones(cost.d + 1)
+    optimal_weights, iters = stoch_grad_desc(cost, initial_weights)
 
 
 if __name__ == '__main__':
-    solve()
+    test()
+    #solve()
