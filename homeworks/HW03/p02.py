941b3734b77232965b21307445d849bfcb8878eb
Jacob Raymond <raymond.jacob17@gmail.com>
2021-12-16 00:22:13 -0500

    Finish implementation for problem 2

--- homeworks/HW03/p02.py
+++ homeworks/HW03/p02.py
@@ -33,13 +33,13 @@ phi = (np.sqrt(5) + 1) / 2
 def golden_search(f, s, e, epsilon=1e-10):
     while (e - s) > epsilon:
         delta = (e - s) / phi
-        a = s + delta
-        b = e - delta
+        a = e - delta
+        b = s + delta
         if f(a) < f(b):
             e = b
         else:
             s = a
-    return (a + b) / 2
+    return (s + e) / 2
 
 
 def grad_desc_opt(f, x_0, *, epsilon=1e-10, maxiters=1000000):
@@ -47,7 +47,7 @@ def grad_desc_opt(f, x_0, *, epsilon=1e-10, maxiters=1000000):
     grad_x = f.grad(x)
     for i in range(maxiters):
         alpha_objective = lambda alpha: f(x - alpha * grad_x)
-        alpha = golden_search(alpha_objective, 0, 1)
+        alpha = golden_search(alpha_objective, 0, 1, epsilon=epsilon)
         x = x - alpha * grad_x
         grad_x = f.grad(x)
         if la.norm(grad_x) < epsilon:
@@ -72,34 +72,69 @@ class Rosenbrock:
         return np.array((2 * (x - self.a) - 4 * x * c, 2 * c))
 
 
+class Himmelblau:
+    def __call__(self, X):
+        x, y = X
+        a = x * x + y - 11
+        b = x + y * y - 7
+        return a * a + b * b
+
+    def grad(self, X):
+        x, y = X
+        a = 2 * (x * x + y - 11)
+        b = 2 * (x + y * y - 7)
+        return np.array((2 * a * x + b, a + 2 * y * b))
+
+
 def solve():
     x_0 = np.zeros(2)
     a = 2
-    for b in (0.01, 0.1, 1, 5, 15, 25, 50, 100):
+    epsilon = 1e-7
+    for b in (0.1, 1, 10, 25, 50, 100):
         f = Rosenbrock(a, b)
 
         t = timer()
-        x1, i1 = grad_desc(f.grad, x_0, alpha=0.001)
+        x1, i1 = grad_desc(f.grad, x_0, alpha=0.001, epsilon=epsilon)
         t1 = t.get_time()
 
         t = timer()
-        x2, i2 = grad_desc_opt(f, x_0)
+        x2, i2 = grad_desc_opt(f, x_0, epsilon=epsilon)
         t2 = t.get_time()
         print(f'=== Rosenbrock({a},{b}) ===')
         print(f' - Basic -')
-        print(f'   time: {t1}')
+        print(f'   time: {t1:.4e}')
         print(f'  iters: {i1}')
-        print(f'      x: {x1}')
-        print(f'   f(x): {f(x1)}')
-        print(f'  Df(x): {f.grad(x1)}')
+        print(f'   f(x): {f(x1):.4e}')
         print(f'')
         print(f' - Optimal -')
-        print(f'   time: {t2}')
+        print(f'   time: {t2:.4e}')
         print(f'  iters: {i2}')
-        print(f'      x: {x2}')
-        print(f'   f(x): {f(x2)}')
-        print(f'  Df(x): {f.grad(x2)}')
+        print(f'   f(x): {f(x2):.4e}')
         print(f'')
+
+    f = Himmelblau()
+
+    t = timer()
+    x1, i1 = grad_desc(f.grad, x_0, alpha=0.001, epsilon=epsilon)
+    t1 = t.get_time()
+
+    t = timer()
+    x2, i2 = grad_desc_opt(f, x_0, epsilon=epsilon)
+    t2 = t.get_time()
+
+    print(f'=== Himmelblau ===')
+    print(f' - Basic -')
+    print(f'   time: {t1:.4e}')
+    print(f'  iters: {i1}')
+    print(f'      x: {x}')
+    print(f'   f(x): {f(x1):.4e}')
+    print(f'')
+    print(f' - Optimal -')
+    print(f'   time: {t2:.4e}')
+    print(f'  iters: {i2}')
+    print(f'      x: {x}')
+    print(f'   f(x): {f(x2):.4e}')
+    print(f'')
 
 
 if __name__ == '__main__':
